3fd50832985ce5af05fd76d4cf1a5474507fc777
suhaila12345 <suhaikeeh@gmail.com>
2023-11-14 10:49:16 +0700

    effect

--- src/Page/Login222.jsx
+++ src/Page/Login222.jsx
@@ -1,11 +1,30 @@
 import { Chip } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FormText from "../Component/Login2/FormText";
+import axios from "axios";
 
 export default function Login2() {
+  const [user , setUser] = useState([]);
+
+  useEffect(async ()=>{
+   await axios.get('http://192.168.101.40:5000/api/test')
+    .then((response)=>{
+      const {data} =response;
+      // console.log(data);
+      // console.log(response);
+      setUser(data.result);
+    })
+},[]);
+
+// console.log(user)
+
+
   return (
     <div>
       <FormText/>
+      {user.map((opt,i)=>(
+        <Chip label={opt.email} />
+      ))}
     </div>
   );
 }
