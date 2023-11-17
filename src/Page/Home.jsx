e0bbe3f55e5da75e2f4626c4f40ce436bb2a719d
suhaila12345 <suhaikeeh@gmail.com>
2023-11-17 17:04:01 +0700

    login

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
         
         if (token === "1") {
             console.log("Login");
-        } else {
+        } else { 
             console.log("fail");
             navigate("/login2");
         }
@@ -20,10 +20,40 @@ export default function Home() {
         navigate("/login2");
     };
 
+    const centerText = {
+        textAlign: 'center',
+        margin: '5px 0', // Add margin between paragraphs
+    };
+
+    const horizontalLayout = {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+    };
+
+
+    const paragraphStyle = {
+        ...centerText,
+        margin: '0 10px', // Adjust the horizontal spacing between paragraphs
+    };
+
     return (
         <div>
-            <h1>LIS INSTRUMENTS MAINTENANCE REPORT FORM</h1>
-
+            <h1 style={centerText}>LIS INSTRUMENTS MAINTENANCE REPORT FORM</h1>
+            <div style={horizontalLayout}>
+                <p style={paragraphStyle}>โรงพยาบาล</p>
+                <p style={paragraphStyle}>จังหวัด</p>
+                <p style={paragraphStyle}>วันที่ปฏิบัติงาน</p>
+                <p style={paragraphStyle}>เดือน</p>
+                <p style={paragraphStyle}>ปี</p>
+            </div>
+            <div style={horizontalLayout}>
+                <p style={paragraphStyle}>วันที่ติดตั้ง</p>
+                <p style={paragraphStyle}>เดือน</p>
+                <p style={paragraphStyle}>ปี</p>
+            </div>
             <button onClick={logout}>Log out</button>
         </div>
     );
