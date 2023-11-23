30e4c0fc75f8074eb72ff046b3a4338554e64e29
Nurda1318 <Nurda21089@gmail.com>
2023-11-23 16:16:29 +0700

    23

--- src/Route/index.js
+++ src/Route/index.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Login1 from "../Page/Login1";
 import Login2 from "../Page/Login222";
 import Home from "../Page/Home";
+import Sing from "../Page/Sing";
 
 export default function RoutePage() {
   return (
@@ -10,6 +11,7 @@ export default function RoutePage() {
       <Route path="/" element={<Login1 />} />
       <Route path="/login2" element={<Login2 />} />
       <Route path="/home" element={<Home/>} />
+      <Route path="/Sing" element={<Sing />} />
     </Routes>
   );
 }
